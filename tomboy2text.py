56585015c0614764ff2ddea4d373691a37e8c742
Johannes H. Jensen <joh@pseudoberries.com>
2015-12-26 22:50:20 +0100

    Add command line interface, use python3

--- tomboy2text.py
+++ tomboy2text.py
@@ -1,8 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
+import os
 import re
 import xml.sax
 import dateutil.parser
+
+def lstrip(string, prefix):
+    if string.startswith(prefix):
+        return string[len(prefix):]
+    else:
+        return string
 
 class TomboyContentHandler(xml.sax.ContentHandler):
     def __init__(self):
@@ -20,7 +27,7 @@ class TomboyContentHandler(xml.sax.ContentHandler):
         self.tags = []
 
     def startElement(self, name, attrs):
-        print "startElement:", name
+        print("startElement:", name)
         if name in ('title', 'note-content', 'last-change-date', 'tag'):
             self.in_element = name
 
@@ -45,7 +52,7 @@ class TomboyContentHandler(xml.sax.ContentHandler):
             self.header_level = 2
 
     def endElement(self, name):
-        print "endElement", name
+        print("endElement", name)
         if name in ('title', 'note-content', 'last-change-date', 'tag'):
             self.in_element = None
 
@@ -76,12 +83,12 @@ class TomboyContentHandler(xml.sax.ContentHandler):
         return pre + content + post
 
     def format_line(self, line):
-        print "format_line", self.header_level
+        print("format_line", self.header_level)
         pre = '#' * self.header_level
         return pre + line + "\n"
 
     def characters(self, content):
-        print "characters:", repr(content)
+        print("characters:", repr(content))
         if self.in_element == 'note-content':
             if content == '\n':
                 self.content += self.format_line(self.line)
@@ -89,11 +96,11 @@ class TomboyContentHandler(xml.sax.ContentHandler):
             else:
                 self.line += self.format_characters(content)
         elif self.in_element == 'title':
-            self.title = content
+            self.title += content
         elif self.in_element == 'last-change-date':
             self.last_change = dateutil.parser.parse(content)
         elif self.in_element == 'tag':
-            self.tags.append(content)
+            self.tags.append(lstrip(content, "system:"))
 
 def parse_note(filename):
     handler = TomboyContentHandler()
@@ -105,10 +112,53 @@ def parse_note(filename):
             'last_change': handler.last_change,
             'tags': handler.tags}
 
+def main(args):
+    #print args
 
-print "===================="
+    outdir = None
+    outfile = None
 
-note = parse_note(sys.argv[1])
-for k,v in note.items():
-    print "[{}]".format(k)
-    print v
+    if len(args.notes) > 1:
+        # Directory required for many notes
+        outdir = args.outfile
+        if os.path.exists(args.outfile):
+            assert os.path.isdir(args.outfile)
+        else:
+            # Create directory
+            os.makedirs(args.outfile)
+
+    if os.path.isdir(args.outfile):
+        outdir = args.outfile
+    elif args.outfile == '-':
+        outfile = sys.stdout
+    else:
+        outfile = open(args.outfile, 'w')
+
+    for notefile in args.notes:
+        note = parse_note(notefile)
+        content = note['content']
+        content += u'\n'.join(['@{}'.format(t) for t in note['tags']])
+        content += u'\n'
+
+        if outdir:
+            outfile = os.path.join(outdir, note['title'] + args.suffix)
+            outfile = open(outfile, 'w')
+
+        outfile.write(content)
+        if outfile is not sys.stdout:
+            outfile.close()
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Convert Tomboy notes to text')
+    parser.add_argument('notes', nargs='+', help='Note files')
+    parser.add_argument('-o', '--outfile', default='-',
+            help='Output file or directory (default: stdout)')
+    parser.add_argument('-s', '--suffix', default='.txt',
+            help='Output file suffix (default: %(default)s)')
+
+    args = parser.parse_args()
+
+    main(args)
